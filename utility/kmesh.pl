37f5fe4db26bfb374ee8d356f30a32479c3c497c
Jae-Mo Lihm <jaemolim96@gmail.com>
2018-11-06 16:33:23 +0900

    make kmesh.pl write begin/end kpoints

--- utility/kmesh.pl
+++ utility/kmesh.pl
@@ -41,6 +41,7 @@ if ($numargs==3) {
 
 
 if ($numargs==4) {
+    printf ("begin kpoints \n")
     for ($x=0; $x<$ARGV[0]; $x++) {
 	for ($y=0; $y<$ARGV[1]; $y++) {
 	    for ($z=0; $z<$ARGV[2]; $z++) {
@@ -48,6 +49,7 @@ if ($numargs==4) {
 	    }
 	}
     }
+    printf ("end kpoints \n")
 }
 
 
